d6536bce1eca522214552b8a98402fcb9e626d0c
moijxbwlfjod-design <moijxbwlfjod@gmail.com>
2026-03-11 13:57:27 +0000

    feat: create the index, update and destroy methods

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\UpdateRequest;
 
 class UserController extends Controller
 {
@@ -12,38 +15,21 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json([
+            'user' => Auth::user()
+        ], 200);
+    }
+    public function update(UpdateRequest $request, User $user)
+    {
+        $data = $request->validated();
+        $user->update($data);
+        $user->save();
+        return response()->json(['message' => 'profile updated successfully'], 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function destroy(User $user)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $user->delete();
+        return response()->json(['message' => 'account removed successfully']);   
     }
 }
