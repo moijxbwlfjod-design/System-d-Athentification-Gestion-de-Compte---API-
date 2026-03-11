924924568fe3864dfdefb5e9cc084a51d7eb19a5
moijxbwlfjod-design <moijxbwlfjod@gmail.com>
2026-03-11 13:59:07 +0000

    feat: add index, update and destroy methods routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,9 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/profile/{user}', [UserController::class, 'index']);
+    Route::put('/profile/{user}', [UserController::class, 'update']);
+    Route::delete('/profile/{user}', [UserController::class, 'destroy']);
+});
