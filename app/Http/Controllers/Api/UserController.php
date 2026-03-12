71b0d1cb2b9e133f2b3d5f80627bb5a7df2dd561
moijxbwlfjod-design <moijxbwlfjod@gmail.com>
2026-03-12 14:33:32 +0000

    fix: now the user can update and delete here account only

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\UpdateRequest;
@@ -16,20 +17,21 @@ class UserController extends Controller
     public function index()
     {
         return response()->json([
-            'user' => Auth::user()
+            'id' => Auth::user()->id,
+            'name' => Auth::user()->name,
+            'email' => Auth::user()->email
         ], 200);
     }
-    public function update(UpdateRequest $request, User $user)
+    public function update(UpdateRequest $request)
     {
         $data = $request->validated();
-        $user->update($data);
-        $user->save();
+        Auth::user()->update($data);
         return response()->json(['message' => 'profile updated successfully'], 200);
     }
 
-    public function destroy(User $user)
+    public function destroy()
     {
-        $user->delete();
+        Auth::user()->delete();
         return response()->json(['message' => 'account removed successfully']);   
     }
 }
